b16ab3ead17ee8180fca9a1fcfcd4bb7aa5cf5c8
ashley-chiteri <ochamiashley97@gmail.com>
2025-09-17 14:33:10 +0300

    Added a new whatsapp number

--- src/components/WhatsAppbutton.tsx
+++ src/components/WhatsAppbutton.tsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/254794396782"
+      href="https://wa.me/254722441772"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 p-4 rounded-full shadow-lg hover:scale-110 hover:bg-green-600 transition-colors z-50"
